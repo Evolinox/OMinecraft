dcdf950110e5f361d4900672dc627408fb7d3ce2
Pascal <pascal.jedicke@icloud.com>
2024-01-04 12:01:18 +0100

    Update

---
+++ src/main/java/com/ominecraftbot/ominecraftbot/OMinecraft_Bot.java
@@ -0,0 +1,17 @@
+package com.ominecraftbot.ominecraftbot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class OMinecraft_Bot extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
